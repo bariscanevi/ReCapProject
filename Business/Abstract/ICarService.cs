876ff3f1a8e75c3022d1558871776cfad8167aca
Barış Canevi <bariscanevi@gmail.com>
2021-02-10 01:33:55 +0300

    Added Core and Updated

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,6 +14,7 @@ namespace Business.Abstract
         List<Car> GetAll();
         List<Car> GetCarsByColorId(int Id);
         List<Car> GetCarsByBrandId(int Id);
+        List<CarDetailDto> GetCarDetails();
 
     }
 }
--- Console/Program.cs
+++ Console/Program.cs
@@ -10,31 +10,73 @@ namespace Console
         static void Main(string[] args)
         {
             BrandManager brandManager = new BrandManager(new EfBrandDal());
-            System.Console.WriteLine("BrandId si 2 olan araçlar");
             CarManager carManager = new CarManager(new EfCarDal());
-            foreach (var car in carManager.GetCarsByBrandId(2))
-            {
-                System.Console.WriteLine(car.Description + " " + car.DailyPrice + " " +car.ModelYear);
-            }
+            ColorManager colorManager = new ColorManager(new EfColorDal());
 
-            System.Console.WriteLine("ColorId si 1 olan araçlar");
-            foreach (var car1 in carManager.GetCarsByColorId(1))
-            {
-                System.Console.WriteLine(car1.Description+car1.ModelYear);
-            }
+            //GetByBrandId(carManager);
 
-            carManager.Add(new Car { BrandId = 3, ColorId = 1, DailyPrice = 500, ModelYear = 2020, Description = "320D" });
+            //GetCarsByColorId(carManager);
+
+            //brandManager.Add(new Brand { BrandName = "SKODA" });
+
+            //BrandGetAll(brandManager);
+
+            //carManager.Delete(new Car { CarId = 6 });
+
+            //carManager.Add(new Car { BrandId = 3, ColorId = 1, DailyPrice = 500, ModelYear = 2020, Description = "320D" });
+
+            //brandManager.Delete(new Brand { BrandName = "OPEL" });
+
+            //colorManager.Update(new Color { Id = 3, ColorName = "KIRMIZI" });
+
+            //carManager.Update(new Car { CarId = 2, BrandId = 1, ColorId = 2, DailyPrice = 520, ModelYear = 2019, Description = "C200D" });
+
+            //CarGetAll(carManager);
+
+            GetCarDetails(carManager);
+
+        }
+
+        private static void GetCarDetails(CarManager carManager)
+        {
+            foreach (var item in carManager.GetCarDetails())
+            {
+                System.Console.WriteLine(item.BrandName + " " + item.Description + " " + item.ColorName + " " + item.DailyPrice);
+            }
+        }
+
+        private static void CarGetAll(CarManager carManager)
+        {
             foreach (var car2 in carManager.GetAll())
             {
-                System.Console.WriteLine(car2.Description);
+                System.Console.WriteLine(car2.Description + " "+ car2.DailyPrice);
             }
+        }
 
-            brandManager.Add(new Brand { BrandName = "OPEL" });
+        private static void BrandGetAll(BrandManager brandManager)
+        {
             foreach (var brand in brandManager.GetAll())
             {
                 System.Console.WriteLine(brand.BrandName);
             }
+        }
 
+        private static void GetCarsByColorId(CarManager carManager)
+        {
+            System.Console.WriteLine("ColorId si 1 olan araçlar");
+            foreach (var car1 in carManager.GetCarsByColorId(1))
+            {
+                System.Console.WriteLine(car1.Description + car1.ModelYear);
+            }
+        }
+
+        private static void GetByBrandId(CarManager carManager)
+        {
+            System.Console.WriteLine("BrandId si 2 olan araçlar");
+            foreach (var car in carManager.GetCarsByBrandId(2))
+            {
+                System.Console.WriteLine(car.Description + " " + car.DailyPrice + " " + car.ModelYear);
+            }
         }
     }
 }
